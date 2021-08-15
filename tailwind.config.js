fa4bd82b8e1f0f76d6096c04778e5096e5c5a4fa
Md Adil <md-adil@live.com>
2021-08-15 19:44:53 +0530

    finishging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,19 @@
-const colors = require('tailwindcss/colors')
-const primary = colors.blue;
+const colors = require("tailwindcss/colors");
+const primary = colors.indigo;
 module.exports = {
-  purge: [
-    "./index.html",
-    "./src/**/*.html"
-  ],
+  purge: ["./index.html", "./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: primary["500"],
         "primary-light": primary["300"],
-        "primary-dark": primary["700"]
-      }
+        "primary-dark": primary["700"],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
